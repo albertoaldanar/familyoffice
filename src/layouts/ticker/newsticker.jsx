11d93c077df720ebe6df4d0127acbff032f47563
albertoaldanar <albertoaldanar@hotmail.com>
2024-10-13 19:57:28 -0700

    finished mocked data

--- src/layouts/ticker/newsticker.jsx
+++ src/layouts/ticker/newsticker.jsx
@@ -1,139 +1,55 @@
-import React, { Fragment } from 'react'
-import Slider from 'react-slick'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
-import { Swiper, SwiperSlide } from 'swiper/react'
+import React, { Fragment } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { sliderData } from "./sliderData";
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { EffectFade, Navigation, Pagination } from "swiper";
 
-// Import Swiper styles
-import 'swiper/css'
-// Import Swiper styles
-// import 'swiper/css'
-import 'swiper/css/effect-fade'
-import 'swiper/css/navigation'
-import 'swiper/css/pagination'
 
-import { EffectFade, Navigation, Pagination } from 'swiper'
-function Newsticker () {
+function Newsticker() {
   const settings = {
-    dots: false,
+    dots: true,
     infinite: true,
     slidesToShow: 9,
     slidesToScroll: 1,
     autoplay: true,
-    speed: 3000,
+    speed: 3500,
     autoplaySpeed: 2000,
-    cssEase: 'linear'
-  }
+    cssEase: "linear",
+  };
+
   return (
     <Fragment>
-     
-   
-      <div className="container-fluid bg-white news-ticker">
-        <div className="bg-white">
-          <div className="best-ticker" >
+      <div className="container-fluid  news-ticker">
+        <div>
+          <div className="best-ticker">
             <div className="bn-news">
-                <ul>
-              <Slider {...settings}>
-                <li className="text-muted fs-13 fw-semibold">
-                  <span className="fa fa-users bg-danger-transparent text-danger mx-1"></span>
-                  <span className="d-inline-block">Miembros familiares</span>
-                  <span className="bn-positive me-4"> 6</span>
-                </li>
-
-                <li className="text-muted fs-13 fw-semibold">
-                  <span className="fa fa-signal bg-info-transparent text-info mx-1"></span>
-                  <span className="d-inline-block">Total patrimonio</span>
-                  <span className="bn-negative me-4"> $ 63,000,000</span>
-                </li>
-
-                <li className="text-muted fs-13 fw-semibold">
-                  <span className="fa fa-briefcase bg-success-transparent text-success mx-1"></span>
-                  <span className="d-inline-block"> Total Trials </span>
-                  <span className="bn-negative me-4">12,765</span>
-                  </li>
-
-                <li className="text-muted fs-13 fw-semibold">
-                  <span className="fa fa-trophy bg-warning-transparent text-warning mx-1"></span>
-                  <span className="d-inline-block">Total Wins</span>
-                  <span className="bn-positive me-4">24</span>
-                  </li>
-
-                <li className="text-muted fs-13 fw-semibold">
-                  <span className="fa fa-envelope bg-primary-transparent text-primary mx-1"></span>
-                  <span className="d-inline-block">Active Email Accounts</span>
-                  <span className="bn-positive me-4">74,526</span>
-                  </li>
-
-                <li className="text-muted fs-13 fw-semibold">
-                  <span className="fa fa-check-circle bg-danger-transparent text-danger mx-1"></span>
-                  <span className="d-inline-block">Active Requests</span>
-                  <span className="bn-positive me-4">14,526</span>
-                  </li>
-
-                <li className="text-muted fs-13 fw-semibold">
-                  <span className="fa fa-envelope bg-secondary-transparent text-secondary mx-1"></span>
-                  <span className="d-inline-block">Deactive Email Accounts</span>
-                  <span className="bn-positive me-4">7,325 </span>
-                </li>
-
-                <li className="text-muted fs-13 fw-semibold">
-                  <span className="fa fa-times-circle bg-info-transparent text-info mx-1"></span>
-                  <span className="d-inline-block">Deactive Requests</span>
-                  <span className="bn-positive me-4"> 1,425 </span>
-                  </li>
-
-                <li className="text-muted fs-13 fw-semibold">
-                  <span className="fa fa-usd bg-success-transparent text-success mx-1"></span>
-                  <span className="d-inline-block">Total Balance</span>
-                  <span className="bn-negative me-4">$1,52,654</span>
-                  </li>
-
-                <li className="text-muted fs-13 fw-semibold">
-                  <span className="fa fa-shopping-cart bg-danger-transparent text-danger mx-1"></span>
-                  <span className="d-inline-block">Total Sales</span>
-                  <span className="bn-negative me-4">23,15,2654</span>
-                  </li>
-
-                <li className="text-muted fs-13 fw-semibold">
-                  <span className="fa fa-money bg-warning-transparent text-warning"></span>
-                  <span className="d-inline-block">Total Purchase</span>
-                  <span className="bn-positive me-4">$7,483</span>
-                  </li>
-
-                <li className="text-muted fs-13 fw-semibold">
-                  <span className="fa fa-usd bg-danger-transparent text-danger mx-1"></span>
-                  <span className="d-inline-block">Total Cost Reduction</span>
-                  <span className="bn-negative me-4">$23,567</span>
-                  </li>
-
-                <li className="text-muted fs-13 fw-semibold">
-                  <span className="fa fa-money bg-primary-transparent text-primary mx-1"></span>
-                  <span className="d-inline-block">Total Cost Savings</span>
-                  <span className="bn-negative me-4">15.2%</span>
-                  </li>
-
-                <li className="text-muted fs-13 fw-semibold">
-                  <span className="fa fa-briefcase bg-info-transparent text-info mx-1"></span>
-                  <span className="d-inline-block">Total Projects</span>
-                  <span className="bn-positive me-4">3,456</span>
-                  </li>
-
-                <li className="text-muted fs-13 fw-semibold">
-                  <span className="fa fa-users bg-success-transparent text-success mx-1"></span>
-                  <span className="d-inline-block">Total Employes</span>
-                  <span className="bn-positive me-4">4,738</span>
-                  </li>
-                  </Slider> 
-                  </ul>
+              <ul>
+                <Slider {...settings}>
+                  {sliderData.map((item, index) => (
+                    <li key={index} className="text-muted fs-12 fw-semibold mx-3">
+                      <span className={`mx-1 fe ${item.icon}`} style={{ color: "#1c2b2e"}}></span>
+                      <span className=" mx-1 d-inline-block" style={{ color: "#1c2b2e" }}>
+                        {item.title}:
+                      </span>
+                      <span className="bn-positive me-4" style={{ color: "#1c2b2e" }}>
+                        {item.value}
+                      </span>
+                    </li>
+                  ))}
+                </Slider>
+              </ul>
             </div>
           </div>
         </div>
       </div>
-   
-
     </Fragment>
-  )
+  );
 }
 
-export default Newsticker
-
+export default Newsticker;
---
+++ src/layouts/ticker/sliderData.js
@@ -0,0 +1,77 @@
+export const sliderData = [
+    {
+        title: 'Miembros Familiares',
+        value: 6, 
+        icon: 'fe-users'
+    }, 
+    {
+        title: 'Total valor de activos',
+        value: '$ 4,500,000.00 MXN', 
+        icon: 'fe-trending-up'
+    }, 
+    {
+        title: 'Total valor empresas',
+        value: '$ 3,700,000.00 MXN', 
+        icon: 'fe-breifcase'
+    }, 
+    {
+        title: 'Total contenido fadeicomisos',
+        value: '$ 1,700,000.00 MXN', 
+        icon: 'fe-layers'
+    },
+    {
+        title: 'Centas bancarias',
+        value: '$ 2,900,000.00 MXN', 
+        icon: 'fe-credit-card'
+    },
+    {
+        title: 'Bienes raices',
+        value: '$ 3,150,000.00 MXN', 
+        icon: 'fe-map-pin'
+    },
+    {
+        title: 'Prestamos por cobrar',
+        value: '$ 150,000.00 MXN', 
+        icon: 'fe-arrow-down-right'
+    },
+    {
+        title: 'Valor en vehiculos',
+        value: '$ 2,450,300.00 MXN', 
+        icon: 'fe-truck'
+    },
+    {
+        title: 'Valor en bursatil',
+        value: '$ 930,300.00 MXN', 
+        icon: 'fe-trending-up'
+    },
+    {
+        title: 'Capital privado',
+        value: '$ 1,130,300.00 MXN', 
+        icon: 'fe-activity'
+    },
+    {
+        title: 'Arte y coleccioes',
+        value: '$ 390,700.00 MXN', 
+        icon: 'fe-watch'
+    },
+    {
+        title: 'Miembros comité inversion',
+        value: 5, 
+        icon: 'fe-users'
+    },
+    {
+        title: 'Miembros consejo familiar',
+        value: 5, 
+        icon: 'fe-users'
+    },
+    {
+        title: 'Miembros VFO',
+        value: 6, 
+        icon: 'fe-users'
+    },
+    {
+        title: 'Numero de proveedores',
+        value: 12, 
+        icon: 'fe-users'
+    }
+];
